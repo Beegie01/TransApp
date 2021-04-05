69b801287e63df94de895a72d8fab31fae724d12
Osagie Aibangbee <76821049+Beegie01@users.noreply.github.com>
2021-04-05 08:44:35 +0100

    Add files via upload

--- GUI/customer_cls.py
+++ GUI/customer_cls.py
@@ -4,12 +4,11 @@ class Customer:
     def __init__(self):
         self.customer_id = None
         self.first_name = None
+        self.mid_name = None
         self.last_name = None
         self.gender = None
         self.date_of_birth = None
-        self.birthday = None
         self.country = None
-        self.state = None
+        self.state_of_origin = None
         self.office_addr = None
         self.date_opened = None
-        self.time_opened = None
